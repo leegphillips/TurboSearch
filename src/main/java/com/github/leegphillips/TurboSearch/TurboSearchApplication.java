55148bcd5af58f5141b30c926a730cb60efb73c5
leegphillips <leegphillips@yahoo.co.uk>
2017-09-26 10:39:13 +0100

    Initial add from Spring Initializr

---
+++ src/main/java/com/github/leegphillips/TurboSearch/TurboSearchApplication.java
@@ -0,0 +1,12 @@
+package com.github.leegphillips.TurboSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurboSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurboSearchApplication.class, args);
+	}
+}
