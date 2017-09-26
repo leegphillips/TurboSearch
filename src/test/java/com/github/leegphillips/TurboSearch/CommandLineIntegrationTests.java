49bdd85d0546777a763deccdbbd168eeba07ba16
leegphillips <leegphillips@yahoo.co.uk>
2017-09-26 12:04:02 +0100

    Added first test skeleton  and travis connector

---
+++ src/test/java/com/github/leegphillips/TurboSearch/CommandLineIntegrationTests.java
@@ -0,0 +1,22 @@
+package com.github.leegphillips.TurboSearch;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class CommandLineIntegrationTests {
+
+    private final static String FILE_CONTENTS = "the quick brown fox jumped over the lazy dog";
+
+    @BeforeClass
+    public static void createFilesToSearch() {
+
+    }
+
+    @Test
+    public void happyPath() {
+
+    }
+}
